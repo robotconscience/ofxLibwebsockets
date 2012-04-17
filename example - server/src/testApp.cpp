dfcffadff6753b0d6ce42d3581e0c9cc9b6321cf
Brett Renfer <brett@robotconscience.com>
2012-04-16 21:39:17 -0400

    beginning of server example

---
+++ example - server/src/testApp.cpp
@@ -0,0 +1,91 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+    bool connected = server.setup( 9091 );
+    server.addListener(this);
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::onConnect( ofxWebSocketEvent& args ){
+    cout<<"on connected"<<endl;
+}
+
+//--------------------------------------------------------------
+void testApp::onOpen( ofxWebSocketEvent& args ){
+    cout<<"on open"<<endl;
+}
+
+//--------------------------------------------------------------
+void testApp::onClose( ofxWebSocketEvent& args ){
+    cout<<"on close"<<endl;
+}
+
+//--------------------------------------------------------------
+void testApp::onIdle( ofxWebSocketEvent& args ){
+    cout<<"on idle"<<endl;
+}
+
+//--------------------------------------------------------------
+void testApp::onMessage( ofxWebSocketEvent& args ){
+    cout<<"got message "<<args.message<<endl;
+}
+
+//--------------------------------------------------------------
+void testApp::onBroadcast( ofxWebSocketEvent& args ){
+    cout<<"got broadcast "<<args.message<<endl;    
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
