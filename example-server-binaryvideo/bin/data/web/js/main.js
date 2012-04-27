1407f312683b1ae6751e875e52ecfef7ec55442d
robotconscience <brett@robotconscience.com>
2012-04-27 10:43:00 -0400

    new example: video streaming over binary

---
+++ example-server-binaryvideo/bin/data/web/js/main.js
@@ -0,0 +1,143 @@
+// GLOBAL VARS
+var socket;
+var canvas;
+var ctx;
+
+var canvasData;
+var data;
+var type;
+
+//----------------------------------------------------------------------------------------------------------------
+// ON READY, SETUP SOCKET!
+//----------------------------------------------------------------------------------------------------------------
+
+$(document).ready( function() {
+	canvas 		= document.getElementById('contourCanvas');
+	ctx			= canvas.getContext('2d');
+
+    canvasData = ctx.getImageData(0,0,canvas.width, canvas.height);
+    data = canvasData.data;
+
+	document.getElementById("brow").textContent = " " + BrowserDetect.browser + " "
+		+ BrowserDetect.version +" " + BrowserDetect.OS +" ";
+
+	setupSocket();
+});
+
+//----------------------------------------------------------------------------------------------------------------
+// WS:ON OPEN
+//----------------------------------------------------------------------------------------------------------------
+function onOpen(){
+	document.getElementById("wslm_statustd").style.backgroundColor = "#40ff40";
+	document.getElementById("wslm_statustd").textContent = " websocket connection opened ";
+}
+
+//----------------------------------------------------------------------------------------------------------------
+// WS: ON CLOSE
+//----------------------------------------------------------------------------------------------------------------
+function onClose(){
+	document.getElementById("wslm_statustd").style.backgroundColor = "#ff4040";
+	document.getElementById("wslm_statustd").textContent = " websocket connection CLOSED ";
+}
+
+//----------------------------------------------------------------------------------------------------------------
+// WS: ON MESSAGE
+//----------------------------------------------------------------------------------------------------------------
+function onMessage( messageEvent ){
+	// check for binary
+	if (messageEvent.data instanceof ArrayBuffer) {
+		var image = new Image();
+		data = canvasData.data;
+
+		var bytearray = new Uint8Array( messageEvent.data );
+		var index = 0;
+
+		// b & w
+		if ( type == 0 ){
+			for (var i = 0; i < data.length; i+=4) {
+            	data[i] = bytearray[index]; 
+	            data[i + 1] = bytearray[index];
+	            data[i + 2] = bytearray[index];
+	            data[i + 3] = 255;
+	            index++;
+			}
+		// rgb
+		} else if ( type == 1 ){
+			for (var i = 0; i < data.length; i+=4) {
+	            data[i] = bytearray[index]; index++;
+	            data[i + 1] = bytearray[index]; index++;
+	            data[i + 2] = bytearray[index]; index++;
+	            data[i + 3] = 255;
+			}
+		// rgba
+		} else {
+			console.log( bytearray.length );
+			console.log( data.length );
+			for (var i = 0; i < bytearray.length; i++) {
+	            data[i] = bytearray[i]
+			}
+		}
+
+        ctx.putImageData(canvasData,0,0);
+	} else if (messageEvent.data instanceof Blob) {
+		var image = new Image();
+		image.onload = function () {
+			ctx.clearRect(0, 0, destinationCanvas.width, destinationCanvas.height);
+			ctx.drawImage(image, 0, 0);
+			document.body.appendChild(image);
+		}
+
+		if ( window.URL ){
+			image.src = window.URL.createObjectURL(messageEvent.data);
+		} else if ( window.webkitURL ){
+			image.src = window.webkitURL.createObjectURL(messageEvent.data);
+		} else {
+			console.error( "your browser does not have a window.URL method :(")
+		}
+	} else {
+		// here's where we'll catch the image stuff!
+
+		var imgData;
+		try {
+			imgData =  jQuery.parseJSON( messageEvent.data );
+		} catch( e ){
+			imgData = messageEvent.data;
+			var vals = imgData.split(":");
+			try {
+				if ( canvas.width != vals[0] || canvas.height != vals[1] ){
+					canvas.width = vals[0];
+					canvas.height = vals[1];
+				    canvasData = ctx.getImageData(0,0,canvas.width, canvas.height);
+				    data = canvasData.data;
+				}
+				type = vals[2];
+			} catch ( e ){
+
+			}
+		}
+	}
+}
+
+//----------------------------------------------------------------------------------------------------------------
+// setup web socket
+//----------------------------------------------------------------------------------------------------------------
+
+function setupSocket(){
+	// setup!
+	if (BrowserDetect.browser == "Firefox") {
+		socket = new MozWebSocket( get_appropriate_ws_url(), "of-protocol" );
+		socket.binaryType = "arraybuffer";
+	} else {
+		socket = new WebSocket( get_appropriate_ws_url(), "of-protocol");	
+		socket.binaryType = "arraybuffer";
+	}
+	
+	// open
+	try {
+		socket.onopen 		= onOpen;
+		socket.onmessage 	= onMessage;
+		socket.onclose 	 	= onClose;
+	} catch(exception) {
+		alert('Error' + exception);  
+	}
+}
---
+++ example-server-binaryvideo/src/testApp.cpp
@@ -0,0 +1,141 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+    // setup a server with default options on port 9092
+    // - pass in true after port to set up with SSL
+    //bool connected = server.setup( 9092 );
+    
+    // Uncomment this to set up a server with a protocol
+    // Right now, clients created via libwebsockets that are connecting to servers
+    // made via libwebsockets seem to want a protocol. Hopefully this gets fixed, 
+    // but until now you have to do something like this:
+    
+    //setup video grabber
+    video.listDevices();
+    bVideoSetup = video.initGrabber( 320, 240 );
+    
+    ofxLibwebsockets::ServerOptions options = ofxLibwebsockets::defaultServerOptions();
+    options.port = 9093;
+    options.protocol = "of-protocol";
+    bool connected = server.setup( options );
+    
+    // this adds your app as a listener for the server
+    server.addListener(this);
+    
+    // setup message queue
+    font.loadFont("myriad.ttf", 20);
+    messages.push_back("WebSocket server setup at "+ofToString( server.getPort() ) + ( server.usingSSL() ? " with SSL" : " without SSL") );
+    
+    ofBackground(0);
+    ofSetFrameRate(30);
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+    video.update();
+    if ( bVideoSetup && video.isFrameNew() ){
+        server.send( ofToString(video.width) +":"+ ofToString( video.height ) +":"+ ofToString( 1 ) );
+        server.sendBinary( video );
+        //messages.push_back( "Sending image" );
+    }
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+    int x = font.getSize();
+    int y = font.getSize()*2;
+    ofSetColor(255);
+    for (int i = messages.size() -1; i >= 0; i-- ){
+        //font.drawString( messages[i], x, y );
+        y += font.stringHeight( messages[i] ) + font.getSize();
+    }
+    if ( bVideoSetup ) video.draw(0,0);
+}
+
+//--------------------------------------------------------------
+void testApp::onConnect( ofxLibwebsockets::Event& args ){
+    cout<<"on connected"<<endl;
+}
+
+//--------------------------------------------------------------
+void testApp::onOpen( ofxLibwebsockets::Event& args ){
+    cout<<"new connection open"<<endl;
+    messages.push_back("New connection from " + args.conn.getClientIP() );
+}
+
+//--------------------------------------------------------------
+void testApp::onClose( ofxLibwebsockets::Event& args ){
+    cout<<"on close"<<endl;
+    messages.push_back("Connection closed");
+}
+
+//--------------------------------------------------------------
+void testApp::onIdle( ofxLibwebsockets::Event& args ){
+    cout<<"on idle"<<endl;
+}
+
+//--------------------------------------------------------------
+void testApp::onMessage( ofxLibwebsockets::Event& args ){
+    cout<<"got message "<<args.message<<endl;
+    
+    // trace out string messages or JSON messages!
+    if ( args.json != NULL){
+        messages.push_back("New message: " + args.json.toStyledString() + " from " + args.conn.getClientName() );
+    } else {
+        messages.push_back("New message: " + args.message + " from " + args.conn.getClientName() );
+    }
+    
+    //if (messages.size() > NUM_MESSAGES) messages.erase( messages.begin() );
+    
+    // echo server = send message right back!
+    args.conn.send( args.message );
+}
+
+//--------------------------------------------------------------
+void testApp::onBroadcast( ofxLibwebsockets::Event& args ){
+    cout<<"got broadcast "<<args.message<<endl;    
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){}
