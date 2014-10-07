99f5d15cc43dde2756093530a7ae5adcba4b6bb1
Brett Renfer <brett@robotconscience.com>
2014-10-07 11:20:22 -0400

    Added actual functionality to iOS example

--- example_client_ios/src/ofApp.h
+++ example_client_ios/src/ofApp.h
@@ -4,6 +4,8 @@
 #include "ofxiOS.h"
 #include "ofxiOSExtras.h"
 
+#include "ofxLibwebsockets.h"
+
 class ofApp : public ofxiOSApp {
 	
     public:
@@ -23,6 +25,16 @@ class ofApp : public ofxiOSApp {
         void gotMemoryWarning();
         void deviceOrientationChanged(int newOrientation);
 
+    
+        ofxLibwebsockets::Client client;
+        
+        // websocket methods
+        void onConnect( ofxLibwebsockets::Event& args );
+        void onOpen( ofxLibwebsockets::Event& args );
+        void onClose( ofxLibwebsockets::Event& args );
+        void onIdle( ofxLibwebsockets::Event& args );
+        void onMessage( ofxLibwebsockets::Event& args );
+        void onBroadcast( ofxLibwebsockets::Event& args );
 };
 
 
--- example_client_ios/src/ofApp.mm
+++ example_client_ios/src/ofApp.mm
@@ -1,18 +1,56 @@
 #include "ofApp.h"
 
-//--------------------------------------------------------------
-void ofApp::setup(){	
+string message = "";
 
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofSetLogLevel(OF_LOG_VERBOSE);
+    client.connect("echo.websocket.org");
+    //    client.connect("echo.websocket.org", true); // optionally use SSL
+    ofSetLogLevel(OF_LOG_ERROR);
+    
+    client.addListener(this);
+    ofSetFrameRate(60);
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-	
+    ofDrawBitmapString("Last message: "+message, ofPoint(0,0));
+}
+
+//--------------------------------------------------------------
+void ofApp::onConnect( ofxLibwebsockets::Event& args ){
+    cout<<"on connected"<<endl;
+}
+
+//--------------------------------------------------------------
+void ofApp::onOpen( ofxLibwebsockets::Event& args ){
+    cout<<"on open"<<endl;
+}
+
+//--------------------------------------------------------------
+void ofApp::onClose( ofxLibwebsockets::Event& args ){
+    cout<<"on close"<<endl;
+}
+
+//--------------------------------------------------------------
+void ofApp::onIdle( ofxLibwebsockets::Event& args ){
+    cout<<"on idle"<<endl;
+}
+
+//--------------------------------------------------------------
+void ofApp::onMessage( ofxLibwebsockets::Event& args ){
+    cout<<"got message "<<args.message<<endl;
+    message = args.message+" at "+ofGetTimestampString();
+}
+
+//--------------------------------------------------------------
+void ofApp::onBroadcast( ofxLibwebsockets::Event& args ){
+    cout<<"got broadcast "<<args.message<<endl;
 }
 
 //--------------------------------------------------------------
@@ -22,7 +60,8 @@ void ofApp::exit(){
 
 //--------------------------------------------------------------
 void ofApp::touchDown(ofTouchEventArgs & touch){
-
+    client.send("Hello");
+    cout << "sending hello" <<endl;
 }
 
 //--------------------------------------------------------------
