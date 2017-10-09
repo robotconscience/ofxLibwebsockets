09ff7a97aee6728b3c697aeba4b365bbbd6a03cd
Brett Renfer <brett@robotconscience.com>
2017-10-09 17:39:10 -0400

    use osx 0.9.8 libs

--- addon_config.mk
+++ addon_config.mk
@@ -124,6 +124,11 @@ osx:
 	ADDON_INCLUDES_EXCLUDE = libs/libwebsockets/include/win32port
 	ADDON_INCLUDES_EXCLUDE += libs/libwebsockets/include/win32port/win32helpers
 
+	# OpenSSL support for OSX 10.11+
+	ADDON_INCLUDES += ../../../libs/openssl/include
+	ADDON_LIBS += ../../../libs/openssl/lib/osx/crypto.a
+    ADDON_LIBS += ../../../libs/openssl/lib/osx/ssl.a
+
 ios:
 	ADDON_INCLUDES_EXCLUDE = libs/libwebsockets/include/win32port/.*
 	ADDON_INCLUDES_EXCLUDE += libs/libwebsockets/include/win32port/win32helpers/.*
